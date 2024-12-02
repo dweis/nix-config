d9a1decaefd91a513e2c4ae184dd120cdd0af343
Derrick Weis <derrick@derrickweis.com>
2024-12-01 22:25:29 -0800

    switch to k3s

--- nixos/kubernetes.nix
+++ nixos/kubernetes.nix
@@ -1,20 +1,15 @@
 {pkgs, ...}: {
-  # Enable virtualbox.
-  #virtualisation.virtualbox.host = {
-  #  enable = true;
-  #  #enableExtensionPack = true;
-  #};
-  #users.extraGroups.vboxusers.members = ["derrick"];
-
-  # Enable docker.
-  virtualisation.docker.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    # ---- Kubernetes / Docker
-    docker
-    #docker-machine
-    #minikube
-    #kubectl
-    #kubernetes-helm
+  networking.firewall.allowedTCPPorts = [
+    6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+    # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+    # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+  ];
+  networking.firewall.allowedUDPPorts = [
+    # 8472 # k3s, flannel: required if using multi-node for inter-node networking
+  ];
+  services.k3s.enable = true;
+  services.k3s.role = "server";
+  services.k3s.extraFlags = toString [
+    # "--debug" # Optionally add additional args to k3s
   ];
 }
